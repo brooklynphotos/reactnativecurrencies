873274c144cbc71cb8e22d7347927574d4f6a793
Brooklyn Photos <guoneng.list@gmail.com>
2018-05-06 23:45:12 -0400

    module 3: homescreen logo

--- app/components/Container/Container.js
+++ app/components/Container/Container.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { View } from 'react-native'
-import styles from './styles'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { View } from 'react-native';
+import styles from './styles';
 
 
 const Container = ({ children }) => (
   <View style={styles.container}>
     {children}
   </View>
-)
+);
 
 Container.protoTypes = {
-  children: PropTypes.any
-}
+  children: PropTypes.any,
+};
 
-export default Container
+export default Container;
--- app/index.js
+++ app/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import EStyleSheet from 'react-native-extended-stylesheet'
-import Home from './screens/Home'
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Home from './screens/Home';
 
 EStyleSheet.build({
-  $primaryBlue: '#4F6D7A'
-})
-export default () => <Home />
+  $primaryBlue: '#4F6D7A',
+  $white: '#fff',
+});
+export default () => <Home />;
